2114d516f74231962d9153de4ce1c0db2bc0a31b
Oleg <olegbackdev@gmail.com>
2025-08-05 16:25:23 +0300

    handle stripe webhook to prevent order data inconsistency during payment process due to closing tab, internet shutdown, etc.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,17 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use App\Models\Product;
+use Exception;
 use Illuminate\Http\Request;
+use Stripe\Exception\SignatureVerificationException;
 use Stripe\StripeClient;
+use Stripe\Webhook;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use UnexpectedValueException;
 
 class ProductController extends Controller
 {
-    public function index(Request $request) {
+    public function index()
+    {
         $products = Product::all();
         return view('product.index', compact('products'));
     }
 
-    public function checkout() {
+    public function checkout()
+    {
         $stripe = new StripeClient(env('STRIPE_SECRET_KEY'));
 
         $products = Product::all();
@@ -40,7 +47,7 @@ class ProductController extends Controller
         $checkout_session = $stripe->checkout->sessions->create([
             'line_items' => $lineItems,
             'mode' => 'payment',
-            'success_url' => route('checkout.success'),
+            'success_url' => route('checkout.success') . "?session_id={CHECKOUT_SESSION_ID}",
             'cancel_url' => route('checkout.cancel'),
         ]);
 
@@ -53,12 +60,80 @@ class ProductController extends Controller
         return redirect($checkout_session->url);
     }
 
-    public function success() {
-        return view('product.checkout-success');
+    /**
+     * @throws Exception
+     */
+    public function success(Request $request)
+    {
+        $stripe = new StripeClient(env('STRIPE_SECRET_KEY'));
+        $sessionId = $request->get('session_id');
+
+        try {
+            $session = $stripe->checkout->sessions->retrieve($sessionId);
+
+            if (!isset($session)) {
+                throw new NotFoundHttpException();
+            }
+            $customer = $session->customer_details;
+
+            $order = Order::where('session_id', $session->id)->first();
+
+            if (!$order) {
+                throw new NotFoundHttpException();
+            }
+
+            if ($order->status === 'unpaid') {
+                $order->status = 'paid';
+                $order->save();
+            }
+
+            return view('product.checkout-success', compact('customer'));
+        } catch (Exception $e) {
+            throw new NotFoundHttpException($e);
+        }
     }
 
-    public function cancel() {
-        return view('product.checkout-success');
+    public function cancel()
+    {
+        //
+    }
 
+    public function webhook()
+    {
+        $endpoint_secret = env('STRIPE_WEBHOOK_SECRET');
+
+        $payload = @file_get_contents('php://input');
+        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+        $event = null;
+
+        try {
+            $event = Webhook::constructEvent(
+                $payload, $sig_header, $endpoint_secret
+            );
+        } catch (UnexpectedValueException $e) {
+            // Invalid payload
+            return response('', 400);
+        } catch (SignatureVerificationException $e) {
+            // Invalid signature
+            return response('', 400);
+        }
+
+        switch ($event->type) {
+            case 'checkout.session.completed':
+                $session = $event->data->object;
+
+                $order = Order::where('session_id', $session->id)->first();
+                if ($order && $order->status === 'unpaid') {
+                    $order->status = 'paid';
+                    $order->save();
+                    // Send email to customer
+                }
+
+            // ... handle other event types
+            default:
+                echo 'Received unknown event type ' . $event->type;
+        }
+
+        return response('');
     }
 }
--- resources/views/product/checkout-success.blade.php
+++ resources/views/product/checkout-success.blade.php
@@ -11,6 +11,6 @@
     class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
 
 <h1>Success</h1>
-
+<h1>{{$customer->name}}</h1>
 </body>
 </html>
